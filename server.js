017960737abafc9115d813b88275593144d43ccf
El Pirata Mike <mikefields81@gmail.com>
2018-04-02 12:49:02 -0500

    cleaning up code

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require("express");
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const logger = require("morgan");
-//dependencies mongoDB
 
-// Require request and cheerio. This makes the scraping possible
 const app = express();
 
 app.use(logger("dev"));
@@ -14,8 +12,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-
-// Initialize Express
 
 
 //Initialize handlebars
