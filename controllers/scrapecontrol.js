ec2e9f4f99f2ed89fdcdb07e83021ea612b660f5
El Pirata Mike <mikefields81@gmail.com>
2018-08-07 18:29:35 -0500

    adding a new user to mlab

--- controllers/scrapecontrol.js
+++ controllers/scrapecontrol.js
@@ -12,7 +12,7 @@ const moment = require('moment');
 
 // Database configuration
 const MONGODB_URI = "mongodb://heroku_194wc5v6:g50o512huirtvbsq75cacv7f8e@ds117539.mlab.com:17539/heroku_194wc5v6";
-const MONGODB_USER = "mongodb://heroku_5n8v1vvx:databa5e@ds041546.mlab.com:41546/heroku_5n8v1vvx"
+const MONGODB_USER = "mongodb://user1:databa5e@ds041546.mlab.com:41546/heroku_5n8v1vvx"
 const databaseUrl = "mongodb://localhost:27017/newsScraper";
 const collections = ["newsscrapes"];
 
